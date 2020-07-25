aea3c2ec6327f21e94654f8da445c014313eafdb
wony0120 <49024958+Jwaminju@users.noreply.github.com>
2020-07-25 09:57:19 +0900

    Week 3
    recursive
    /**/분할 정복

---
+++ Minju/recursive_function/11729-하노이 탑 이동 순서/11729-하노이 탑 이동 순서.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#pragma warning (disable: 4996)
+void hanoiTop(int n, int from, int to, int other) {
+	if (n == 0) { return; }
+	hanoiTop(n - 1, from, other, to
+	);
+	printf("%d %d\n", from, to);
+	hanoiTop(n - 1, other, to, from);	
+}
+
+int main() {
+	 
+	int n, count = 1;
+	scanf("%d", &n);
+	count = (1 << n) - 1;
+	printf("%d\n", count);
+	hanoiTop(n, 1, 3, 2);
+}
